6fc89c82c0c0dbb897a7ddce5e7323e56e4d3dbb
Tinar Saun <tinartinar720@gmail.com>
2024-05-10 04:24:52 +0700

    update-profile

--- app/Http/Controllers/api/PegawaiController.php
+++ app/Http/Controllers/api/PegawaiController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Pegawai;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class PegawaiController extends Controller
 {
@@ -40,21 +42,61 @@ class PegawaiController extends Controller
         $data = $request->all();
 
         $validate = Validator::make($data, [
-            'gaji' => 'required',
-            'bonus_gaji' => 'required',
             'jabatan' => 'required',
-            'id_user' => 'required',
         ]);
 
         if ($validate->fails()) {
             return response(['message' => $validate->errors()->first()], 400);
         }
+        if($data['jabatan'] == 'Owner'){
+            $data['id_role'] = 1;
+        } else if($data['jabatan'] == 'Manager Oprasional'){
+            $data['id_role'] = 2;
+        } else if($data['jabatan'] == 'Admin'){
+            $data['id_role'] = 3;
+        } else {
+            $data['id_role'] = 4;
+        }
+        
 
+        if ($request->hasFile('foto_profile')) {
+            $uploadFolder = 'images';
+            $image = $request->file('foto_profile');
+
+            // Generate nama file acak dengan 12 karakter
+            $randomFileName = Str::random(12);
+
+            // Dapatkan ekstensi file asli
+            $extension = $image->getClientOriginalExtension();
+
+            // Gabungkan nama file acak dengan ekstensi
+            $fileNameToStore = $randomFileName . '.' . $extension;
+
+            // Simpan gambar
+            $image_uploaded_path = $image->storeAs($uploadFolder, $fileNameToStore, 'public');
+
+            // Mendapatkan nama file yang diunggah
+            $uploadedImageResponse = basename($image_uploaded_path);
+
+            // Set data foto profile baru
+            $data['foto_profile'] = 'images/' . $uploadedImageResponse;
+        }
+        // return response([
+        //     'data' => $data
+        // ]);
+
+        $data['password'] = bcrypt($request->password);
+        $user = User::create($data);
+        $data['id_user'] = $user->id_user; 
+
+        $data['gaji'] = 0;
         $pegawai = Pegawai::create($data);
+
 
         return response([
             'message' => 'Pegawai created successfully',
-            'data' => $pegawai
+            'data' => $pegawai,
+            'data' => $user
         ], 200);
     }
 
