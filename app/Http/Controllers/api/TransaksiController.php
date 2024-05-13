d00cd9ed338b8a389b82ab11e328907cf2910228
Exellll <axellio336@gmail.com>
2024-05-13 18:16:32 +0700

    Transaksi

--- app/Http/Controllers/api/TransaksiController.php
+++ app/Http/Controllers/api/TransaksiController.php
@@ -15,9 +15,8 @@ class TransaksiController extends Controller
 {
     public function showAll()
     {
-        $transaksis = Transaksi::select('transaksi.*','users.name_lengkap')
-        ->join('users', 'users.id_user', 'transaksi.id_user')
-        ->where('transaksi.id_user')->get();
+        $transaksis = Transaksi::select('transaksi.*','users.nama_lengkap')
+        ->join('users', 'users.id_user', 'transaksi.id_user')->get();
         foreach ($transaksis as $transaksi) {
             $detail_transaksis = DetailTransaksi::where('id_transaksi', $transaksi->id_transaksi)->get();
             $transaksi->detail_transaksi = $detail_transaksis;
@@ -37,7 +36,7 @@ class TransaksiController extends Controller
     public function showByUser()
     {
         $id_user =  Auth::user()->id_user;
-        $transaksis = Transaksi::select('transaksi.*','users.name_lengkap')
+        $transaksis = Transaksi::select('transaksi.*','users.nama_lengkap')
         ->join('users', 'users.id_user', 'transaksi.id_user')
         ->where('transaksi.id_user', $id_user)->get();
         foreach ($transaksis as $transaksi) {
